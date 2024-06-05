7f3cb4aebcf36998d03ac52df430695fcdaa7150
Jamez <154967862+JamesH200@users.noreply.github.com>
2024-06-05 13:53:49 -0700

    add Css link to index.Html

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+alert('Hello world');
+let favoritefood = pasta 
+let servings = 10;
+let doubleServings = size * 2;
+let minSize = (doubleServerings * 2) - (size / 2);
+document.write(minServings);
