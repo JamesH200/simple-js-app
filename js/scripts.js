eb38c2fbecfa69887e68fe0a9ac697c556eea807
Jamez <154967862+JamesH200@users.noreply.github.com>
2024-06-06 14:35:33 -0700

    removed css form js folder make independent

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
 alert('Hello world');
-let favoritefood = pasta 
-let servings = 10;
-let doubleServings = size * 2;
-let minSize = (doubleServerings * 2) - (size / 2);
+let favoritefood = "pasta"
 document.write(minServings);
