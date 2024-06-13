8fc7e2e3731290d3b7ce93aaa5c10aebfd7e7a64
Jamez <154967862+JamesH200@users.noreply.github.com>
2024-06-13 10:23:31 -0700

    update item list of pokemon with description.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
 let pokemonList = [
-    {id: 1, name: 'chamander', type: ['fire'] },
-    {id: 2, name: 'charmeleon', type: ['fire']},
-    {id: 3, name: 'charizard', type: ['fire','flying']}
+    
+    {
+        id: 1,
+        name: 'Charmeleon',
+        type: ['fire'],
+        height: 1.1
+    },
+    {
+        id: 2, 
+        name: 'Charizard',
+        type: ['fire', 'flying'],
+        height: 1.7
+    },
+    {
+        id: 3, 
+        name: 'Onix',
+        type: ['rock', 'ground'],
+        height: 8.8
+    },
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    document.write(pokemon.name + " (height: " + pokemon.height + ")");
+    if (pokemon.height > 8) {
+        document.write(" - Wow, that\'s Big!");
+    }
+    document.write("</br>");
+};
 
