f1236381c0d71923e9ebdf4e2468ffcd5011c983
Jamez <154967862+JamesH200@users.noreply.github.com>
2024-06-20 16:40:50 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,43 @@
-// Wrap pokemonList array in IIFE
+
 let pokemonRepository = (function () {
-    // pokemonList array
-    let pokemonList = [
-        { name: "Charmeleon", type: ["Fire"], height: 1.1 },
-        { name: "Charizard", type: ["Fire", "Flying"], height: 1.7 },
-        { name: "Onix", type: ["Rock", "Ground"], height: 8.8 },
-        { name: "Bulbasaur", type: ["Grass", "Poison"], height: 0.7 }
-    ];
 
-    // return all items to functions
-    function getAll() {
-        return pokemonList;
-    }
+let pokemonList = [
 
-    // add a single item to functions 
-    function add(pokemon) {
-        pokemonList.push(pokemon);
-    }
+    {
+    
+        name: 'Charmeleon',
+        type: ['fire'],
+        height: 1.1
+    },
+    {
+        
+        name: 'Charizard',
+        type: ['fire', 'flying'],
+        height: 1.7
+    },
+    {
+        
+        name: 'Onix',
+        type: ['rock', 'ground'],
+        height: 8.8
+    },
+]
 
-    // Return an object with the public functions
-    return {
-        getAll: getAll,
-        add: add
-    };
-})();
+function getAll () {
+    return pokemonList;
+}
 
-// Update forEach loop to use pokemonRepository.getAll()
-pokemonRepository.getAll().forEach(function (pokemon) {
-    console.log(pokemon.name);
-});
+function add (pokemon) {
+    pokemonList.push(pokemon);
+}
 
+return{
+    getAll: getAll,
+    add: add
+}
+
+})()
+
+console.log(pokemonRepository.getAll())
 
 
