f96bdc78ab48be96b69fff8cbfa9ae96f9fe79b2
Jamez <154967862+JamesH200@users.noreply.github.com>
2024-06-24 17:15:53 -0700

    update js/script.js with IIFE function

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,38 @@
+
 let pokemonRepository = (function () {
 
     let pokemonList = [
         {
-            id: 1,
             name: 'Charmander',
             type: ['fire'],
             height: 1.1
         },
         {
-            id: 2,
+
             name: 'Charizard',
             type: ['fire', 'flying'],
             height: 1.7
         },
         {
-            id: 3,
+
             name: 'Onix',
             type: ['rock', 'ground'],
             height: 8.8
         }
     ]
 
-    let pokemonRepository = (function () {
-        let pokemonList = []; 
-      
-        return {
-          add: function(pokemon) {
-            pokemonList.push(pokemon);
-          },
-          getAll: function() {
-            return pokemonList;
-          }
-        };
-      })();
-      
-      console.log(pokemonRepository.getAll()); 
-      pokemonRepository.add({ name: 'Onix' });
-      console.log(pokemonRepository.getAll());
+    function getAll() {
+        return pokemonList;
+    }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+
+})()
+
+console.log(pokemonRepository.getAll()) 
